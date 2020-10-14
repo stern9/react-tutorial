16dd334c9c51c64a0308cb8bd5d27681c6e247c9
stern9 <stern9@gmail.com>
2020-10-13 22:23:09 -0600

    Updates

---
+++ precache-manifest.25bf1ac871670033c728d594070f84fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e973eb7002106918f73a51e6d5c1a55",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "277b83218631fa186730",
+    "url": "/react-tutorial/static/css/main.84902bd8.chunk.css"
+  },
+  {
+    "revision": "aef1d30c00621f6e5651",
+    "url": "/react-tutorial/static/js/2.da419696.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.da419696.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "277b83218631fa186730",
+    "url": "/react-tutorial/static/js/main.fd435f32.chunk.js"
+  },
+  {
+    "revision": "d2980c81d16204d1adff",
+    "url": "/react-tutorial/static/js/runtime-main.fba2e450.js"
+  }
+]);
